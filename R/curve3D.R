b51835a72c8732536fab74af502b61417db523df
John Myles White <jmw@johnmyleswhite.com>
2011-03-22 00:05:28 -0400

    First draft of curve3D() function

---
+++ R/curve3D.R
@@ -0,0 +1,23 @@
+curve3D <- function(f, from.x, to.x, from.y, to.y, heatmap = FALSE, n = 101)
+{
+  library('lattice')
+  library('ggplot2')
+  
+	x.seq <- seq(from.x, to.x, (to.x - from.x) / n)
+	y.seq <- seq(from.y, to.y, (to.y - from.y) / n)
+	eval.points <- expand.grid(x.seq, y.seq)
+	names(eval.points) <- c('x', 'y')
+  eval.points <- transform(eval.points,
+                           z = apply(eval.points,
+                                     1,
+                                     function (r) {f(r['x'], r['y'])}))
+  if (heatmap)
+  {
+    p <- ggplot(eval.points, aes(x = x, y = y, fill = z)) + geom_tile()
+    print(p)
+  }
+  else
+  {
+    print(wireframe(z ~ x * y, eval.points))
+  }
+}
